b0c6b052dbcd73089b344da61202ea2913f8882c
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-11-11 19:44:28 +0100

    adds hiding side panel button

--- op.js
+++ op.js
@@ -1,32 +1,114 @@
-const addButtons = function () {
-	// for logging messages from extension
-	const opLog = function (text) {
-		console.log("OPExtensions: " + text);
+// for logging messages from extension
+const opLog = function (text) {
+	console.log("OPExtensions: " + text);
+}
+
+const waitForElements = function (selectors, delay, numberOfTries, callback) {
+	const wait = setInterval(function () {
+		let elements = [];
+		for (let i = 0; i < selectors.length; i++) {
+			let element = document.querySelector(selectors[i]);
+			if (element) {
+				elements.push(element);
+			}
+		}
+
+		numberOfTries -= 1;
+		if (elements.length !== selectors.length) {
+			if (numberOfTries <= 0) {
+				clearInterval(wait);
+			}
+			return;
+		}
+
+		clearInterval(wait);
+		callback(elements);
+	}, delay);
+}
+
+const addHideSidePanelButton = function () {
+	const addArrowButton = function (panels) {
+		let leftPanel = panels[0];
+		let rightPanel = panels[1];
+
+		// found split panels
+		opLog('Detected Open Project Work Package view with split panels - adding hide right side panel button');
+
+		// create arrow button
+		const arrowHolder = document.createElement('div');
+		arrowHolder.id = 'op-extensions-hide-side-panel-arrow-container';
+
+		const rightArrow = document.createElement('a');
+		rightArrow.id = 'op-extensions-hide-side-panel-arrow';
+		rightArrow.classList.add(
+				'navigation-toggler',
+				'icon4',
+				'icon-double-arrow-right'
+			);
+
+		let isRightPanelHidden = false;
+		const switchRightPanelHide = function () {
+			if (isRightPanelHidden) {
+				rightArrow.classList.remove('icon-double-arrow-left');
+				rightArrow.classList.add('icon-double-arrow-right');
+
+				rightPanel.style.minWidth = null;
+				rightPanel.style.width = null;
+				leftPanel.style.width = null;
+			} else {
+				rightArrow.classList.remove('icon-double-arrow-right');
+				rightArrow.classList.add('icon-double-arrow-left');
+
+				rightPanel.style.minWidth = '0px';
+				rightPanel.style.width = '0.001%';
+				leftPanel.style.width = '100%';
+			}
+
+			isRightPanelHidden = !isRightPanelHidden;
+		};
+		rightArrow.addEventListener('click', switchRightPanelHide);
+
+		// add arrow button to the panel
+		arrowHolder.appendChild(rightArrow);
+		leftPanel.insertBefore(arrowHolder, leftPanel.childNodes[0]);
 	}
 
-	// try to find expected "Back" button below diff view
+	// check out if it may be the work package view
+    if(window.location.href.indexOf("work_package") == -1) {
+		// not the work package view - abort
+		return;
+	}
+
+	// probably the work package wiew - try waiting for panels to show up
+	let sidePanelsSelectors = [
+			'.work-packages--left-panel',
+			'.work-packages--right-panel'
+		];
+	waitForElements(sidePanelsSelectors, 200, 50, addArrowButton);
+};
+
+const addDiffButtons = function () {
+	// try to find expected diff view
 	const diffView = document.querySelector('#content > .text-diff');
 
-	// probably not Open Project page - abort plugin execution
+	// probably not Open Project diff view page - abort plugin execution
 	if (!diffView) {
 		return;
 	}
 
 	// found Open Project Diff View
-	opLog("Detected OpenProject Diff View - adding buttons");
+	opLog('Detected OpenProject Diff View - adding buttons');
 
 	// create buttons holder
-	const btnHolder = document.createElement("p");
-	btnHolder.style.margin = "0";
-	btnHolder.style.padding = "0";
+	const btnHolder = document.createElement('p');
+	btnHolder.style.margin = '0';
+	btnHolder.style.padding = '0';
 
 	// add buttons to buttons holder
 	var addButton = function (holder, text, handler) {
-		const button = document.createElement("a");
-
+		const button = document.createElement('a');
+		button.classList.add('op-extensions-diff-button');
 		button.innerText = text
-		button.style.paddingRight = "7px";
-		button.style.paddingLeft  = "7px";
 		button.addEventListener('click', handler);
 
 		holder.appendChild(button);
@@ -43,17 +125,17 @@ const addButtons = function () {
 	}
 
 	const modifyDiffView = function (showBefore, showAfter) {
-		setDisplayFor('ins.diffmod', (showAfter  ? "" : "none"))
-		setDisplayFor('del.diffmod', (showBefore ? "" : "none"))
+		setDisplayFor('ins.diffmod', (showAfter  ? '' : 'none'))
+		setDisplayFor('del.diffmod', (showBefore ? '' : 'none'))
 	};
 
-	addButton(btnHolder, "Before",   modifyDiffView.bind(null, true,  false));
-	addButton(btnHolder, "After",    modifyDiffView.bind(null, false, true));
-	addButton(btnHolder, "Mix view", modifyDiffView.bind(null, true,  true));
+	addButton(btnHolder, 'Before',   modifyDiffView.bind(null, true,  false));
+	addButton(btnHolder, 'After',    modifyDiffView.bind(null, false, true));
+	addButton(btnHolder, 'Mix view', modifyDiffView.bind(null, true,  true));
 
 	// add buttons holder with buttons before diff view
 	diffView.parentNode.insertBefore(btnHolder, diffView);
 };
 
-addButtons();
-
+addDiffButtons();
+addHideSidePanelButton();
