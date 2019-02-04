9a58fdf0f2e89d7e310a8ab1cc75316b8c9dd7b0
gabr <arkadiuszgabrys@gmail.com>
2019-02-04 21:13:43 +0100

    Fixing table of contents when using {{toc}}

--- README.md
+++ README.md
@@ -6,10 +6,29 @@ List of extensions:
 
  - Additional buttons (prev, current, mix) in description diff view
  - Additional button in WP view allowing to fold right panel
+ - Fixes table of contents when using `{{toc}}`
 
 ## Diff View with new buttons
 ![](https://github.com/gabr/opextensions/raw/master/diffView.png)
 
 ## Folding right panel in Work Package view
 ![](https://github.com/gabr/opextensions/raw/master/foldingRightPanel.png)
+
+# TODO
+
+Api: `$HOME\Documents\WindowsPowerShell\Scripts\GetOpenProject-CloseStatusForWP.ps1`
+
+- Auto insert templates in new Work Packages
+- zawsze pokazywanie podpowiedzi składni edytowania
+- zmiana rozmiaru elementów, oba powinny respektować minimalny rozmiar głównej/środkowej części
+    - węższa kolumna aktywności/relacji
+    - węższa kolumna menu po lewej
+- nie wchodzenie w tryb edycji gdy się kliknie w opis
+- akceptowanie edytowanego opisu za pomocą CTRL+S
+- automatyczne przypisywanie mnie do zadania przy tworzeniu nowych
+- automatyczna struktura opisu przy tworzeniu nowych zadań
+- dodanie przycisku, który pozwala od razu utworzyć BUG/TASK by potem nie trzeba było zmieniać typu
+- edytor WYSIWYG
+- wykrywanie konfliktu edycji w trakcie edycji
+- spis treści cały czas widoczny i pokazujący gdzie się znajdujesz w dokumencie
 
--- op.js
+++ op.js
@@ -1,132 +1,189 @@
-// for logging messages from extension
 const opLog = function (text) {
-	console.log("OPExtensions: " + text);
+    console.log("OPExtensions: " + text);
+}
+const opLogError = function (text) {
+    console.error("OPExtensions error: " + text);
 }
 
+
 const waitForElements = function (selectors, delay, numberOfTries, callback) {
-	const selector = selectors.join(',');
+    const selector = selectors.join(',');
 
-	(function check () {
-		const elements = document.querySelectorAll(selectors);
+    (function check () {
+        const elements = document.querySelectorAll(selectors);
 
-		numberOfTries -= 1;
+        numberOfTries -= 1;
 
-		if (elements.length === selectors.length) {
-			callback(elements);
-		} else if (numberOfTries) {
-			setTimeout(check, delay);
-		}
-	})();
+        if (elements.length === selectors.length) {
+            callback(elements);
+        } else if (numberOfTries) {
+            setTimeout(check, delay);
+        }
+    })();
+}
+
+const isOnWorkPackageView = function () {
+    return (window.location.href.indexOf("work_package") !== -1);
 }
 
 const addHideSidePanelButton = function () {
-	const addArrowButton = function ([leftPanel, rightPanel]) {
+    if (isOnWorkPackageView() === false) {
+        // not the work package view - abort
+        return;
+    }
 
-		// found split panels
-		opLog('Detected Open Project Work Package view with split panels - adding hide right side panel button');
+    const addArrowButton = function ([leftPanel, rightPanel]) {
 
-		// create arrow button
-		const arrowHolder = document.createElement('div');
-		arrowHolder.id = 'op-extensions-hide-side-panel-arrow-container';
+        // found split panels
+        opLog('Detected Open Project Work Package view with split panels - adding hide right side panel button');
 
-		const rightArrow = document.createElement('a');
-		rightArrow.id = 'op-extensions-hide-side-panel-arrow';
-		rightArrow.classList.add(
-			'navigation-toggler',
-			'icon4',
-			'icon-double-arrow-right'
-		);
+        // create arrow button
+        const arrowHolder = document.createElement('div');
+        arrowHolder.id = 'op-extensions-hide-side-panel-arrow-container';
 
-		let isRightPanelHidden = false;
-		const switchRightPanelHide = function () {
-			if (isRightPanelHidden) {
-				rightArrow.classList.remove('icon-double-arrow-left');
-				rightArrow.classList.add('icon-double-arrow-right');
+        const rightArrow = document.createElement('a');
+        rightArrow.id = 'op-extensions-hide-side-panel-arrow';
+        rightArrow.classList.add(
+            'navigation-toggler',
+            'icon4',
+            'icon-double-arrow-right'
+        );
 
-				rightPanel.style.minWidth = null;
-				rightPanel.style.width = null;
-				leftPanel.style.width = null;
-			} else {
-				rightArrow.classList.remove('icon-double-arrow-right');
-				rightArrow.classList.add('icon-double-arrow-left');
+        let isRightPanelHidden = false;
+        const switchRightPanelHide = function () {
+            if (isRightPanelHidden) {
+                rightArrow.classList.remove('icon-double-arrow-left');
+                rightArrow.classList.add('icon-double-arrow-right');
 
-				rightPanel.style.minWidth = '0px';
-				rightPanel.style.width = '0.001%';
-				leftPanel.style.width = '100%';
-			}
+                rightPanel.style.minWidth = null;
+                rightPanel.style.width = null;
+                leftPanel.style.width = null;
+            } else {
+                rightArrow.classList.remove('icon-double-arrow-right');
+                rightArrow.classList.add('icon-double-arrow-left');
 
-			isRightPanelHidden = !isRightPanelHidden;
-		};
-		rightArrow.addEventListener('click', switchRightPanelHide);
+                rightPanel.style.minWidth = '0px';
+                rightPanel.style.width = '0.001%';
+                leftPanel.style.width = '100%';
+            }
 
-		// add arrow button to the panel
-		arrowHolder.appendChild(rightArrow);
-		leftPanel.insertBefore(arrowHolder, leftPanel.firstChild);
-	}
+            isRightPanelHidden = !isRightPanelHidden;
+        };
+        rightArrow.addEventListener('click', switchRightPanelHide);
 
-	// check out if it may be the work package view
-	if(window.location.href.indexOf("work_package") === -1) {
-		// not the work package view - abort
-		return;
-	}
+        // add arrow button to the panel
+        arrowHolder.appendChild(rightArrow);
+        leftPanel.insertBefore(arrowHolder, leftPanel.firstChild);
+    }
 
-	// probably the work package wiew - try waiting for panels to show up
-	const sidePanelsSelectors = [
-		'.work-packages--left-panel',
-		'.work-packages--right-panel'
-	];
-	waitForElements(sidePanelsSelectors, 200, 50, addArrowButton);
+    // probably the work package wiew - try waiting for panels to show up
+    const sidePanelsSelectors = [
+        '.work-packages--left-panel',
+        '.work-packages--right-panel'
+    ];
+    waitForElements(sidePanelsSelectors, 200, 50, addArrowButton);
 };
 
 const addDiffButtons = function () {
-	// try to find expected diff view
-	const diffView = document.querySelector('#content > .text-diff');
+    // try to find expected diff view
+    const diffView = document.querySelector('#content > .text-diff');
 
-	// probably not Open Project diff view page - abort plugin execution
-	if (!diffView) {
-		return;
-	}
+    // probably not Open Project diff view page - abort plugin execution
+    if (!diffView) {
+        return;
+    }
 
-	// found Open Project Diff View
-	opLog('Detected OpenProject Diff View - adding buttons');
+    // found Open Project Diff View
+    opLog('Detected OpenProject Diff View - adding buttons');
 
-	// create buttons holder
-	const btnHolder = document.createElement('p');
-	btnHolder.style.margin = '0';
-	btnHolder.style.padding = '0';
+    // create buttons holder
+    const btnHolder = document.createElement('p');
+    btnHolder.style.margin = '0';
+    btnHolder.style.padding = '0';
 
-	// add buttons to buttons holder
-	const addButton = function (holder, text, handler) {
-		const button = document.createElement('a');
-		button.classList.add('op-extensions-diff-button');
-		button.innerText = text;
-		button.addEventListener('click', handler);
+    // add buttons to buttons holder
+    const addButton = function (holder, text, handler) {
+        const button = document.createElement('a');
+        button.classList.add('op-extensions-diff-button');
+        button.innerText = text;
+        button.addEventListener('click', handler);
 
-		holder.appendChild(button);
-	};
+        holder.appendChild(button);
+    };
 
 
-	const setDisplayFor = function (selector, displayValue) {
-		const nodes = diffView.querySelectorAll(selector);
-		if (nodes) {
-			for (let i = 0; i < nodes.length; i++) {
-				nodes[i].style.display = displayValue;
-			}
-		}
-	}
+    const setDisplayFor = function (selector, displayValue) {
+        const nodes = diffView.querySelectorAll(selector);
+        if (nodes) {
+            for (let i = 0; i < nodes.length; i++) {
+                nodes[i].style.display = displayValue;
+            }
+        }
+    }
 
-	const modifyDiffView = function (showBefore, showAfter) {
-		setDisplayFor('ins.diffmod', (showAfter  ? '' : 'none'));
-		setDisplayFor('del.diffmod', (showBefore ? '' : 'none'));
-	};
+    const modifyDiffView = function (showBefore, showAfter) {
+        setDisplayFor('ins.diffmod', (showAfter  ? '' : 'none'));
+        setDisplayFor('del.diffmod', (showBefore ? '' : 'none'));
+    };
 
-	addButton(btnHolder, 'Before',   modifyDiffView.bind(null, true,  false));
-	addButton(btnHolder, 'After',    modifyDiffView.bind(null, false, true));
-	addButton(btnHolder, 'Mix view', modifyDiffView.bind(null, true,  true));
+    addButton(btnHolder, 'Before',   modifyDiffView.bind(null, true,  false));
+    addButton(btnHolder, 'After',    modifyDiffView.bind(null, false, true));
+    addButton(btnHolder, 'Mix view', modifyDiffView.bind(null, true,  true));
 
-	// add buttons holder with buttons before diff view
-	diffView.parentNode.insertBefore(btnHolder, diffView);
+    // add buttons holder with buttons before diff view
+    diffView.parentNode.insertBefore(btnHolder, diffView);
 };
 
+const fixTableOfContents = function () {
+    const fixLinksToHeaders = function ([editor]) {
+        if (!editor) {
+            opLogError('Editor not found');
+            return;
+        }
+
+        const toc = editor.querySelector('.toc');
+        if (!toc) {
+            return;
+        }
+
+        opLog('Table of Contents detected - fixing links to headers');
+
+        const links = editor.querySelectorAll('a');
+        if (!links) {
+            opLogError('Editor not found');
+            return;
+        }
+
+        const urlSplit = document.URL.split("#");
+        const baseUrl = urlSplit[0];
+
+        for (let i = 0; i < links.length; i++) {
+            const href = links[i].getAttribute('href');
+            if (!href || href.startsWith("#") === false) {
+                continue;
+            }
+
+            links[i].setAttribute('href', baseUrl + href);
+
+            const classes = links[i].getAttribute('class');
+            if (classes && classes.includes('wiki-anchor')) {
+                links[i].parentNode.setAttribute('id', href.substring(1))
+            }
+        }
+
+        if (urlSplit.length > 1) {
+            document.getElementById(urlSplit[1]).scrollIntoView();
+        }
+    }
+
+    const detectEditorSelectors = ['#work-package-description'];
+    waitForElements(detectEditorSelectors, 200, 50, fixLinksToHeaders);
+};
+
+
 addDiffButtons();
-addHideSidePanelButton();
+if (isOnWorkPackageView()) {
+    addHideSidePanelButton();
+    fixTableOfContents();
+}
+
